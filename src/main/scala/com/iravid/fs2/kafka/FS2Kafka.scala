f14a6aa4293471a6477a18699969bd88ef17bb67
Itamar Ravid <iravid@iravid.com>
2018-06-01 22:08:29 +0300

    Do some cleanups

--- src/main/scala/com/iravid/fs2/kafka/FS2Kafka.scala
+++ src/main/scala/com/iravid/fs2/kafka/FS2Kafka.scala
@@ -1,8 +1,11 @@
 package com.iravid.fs2.kafka
 
+import cats.MonadError
 import cats.effect._, cats.effect.implicits._, cats.implicits._
 import fs2._
 import java.util.{ Collection => JCollection, Properties }
+import org.apache.kafka.clients.consumer.OffsetAndMetadata
+import org.apache.kafka.clients.consumer.internals.NoOpConsumerRebalanceListener
 import org.apache.kafka.clients.consumer.{ ConsumerRebalanceListener, OffsetCommitCallback }
 import org.apache.kafka.clients.producer.{ Callback, RecordMetadata }
 import org.apache.kafka.common.TopicPartition
@@ -13,17 +16,25 @@ import scala.concurrent.ExecutionContext
 
 object ConsumerCommands {
   case class CommitRequest[F[_]](promise: async.Promise[F, Either[Throwable, Unit]], topic: String,
-    partition: Int, offset: Long)
+    partition: Int, offset: Long) {
+    def asOffsetMap: OffsetMap =
+      Map(new TopicPartition(topic, partition) -> new OffsetAndMetadata(offset))
+  }
+
+  object CommitRequest {
+    def apply[F[_]: Effect](topic: String, partition: Int, offset: Long)(
+      implicit ec: ExecutionContext): F[CommitRequest[F]] =
+      async.Promise.empty[F, Either[Throwable, Unit]]
+        .map(CommitRequest(_, topic, partition, offset))
+  }
+
   case object Poll
 }
 
 case class CommitQueue[F[_]](queue: async.mutable.Queue[F, ConsumerCommands.CommitRequest[F]]) {
-  def requestCommit(topic: String, partition: Int, offset: Long)(implicit F: Effect[F], ec: ExecutionContext): F[Unit] =
-    for {
-      promise <- async.Promise.empty[F, Either[Throwable, Unit]]
-      _ <- queue.enqueue1(ConsumerCommands.CommitRequest(promise, topic, partition, offset))
-      _ <- promise.get
-    } yield ()
+  def requestCommit(request: ConsumerCommands.CommitRequest[F])(
+    implicit F: MonadError[F, Throwable]): F[Unit] =
+      queue.enqueue1(request) *> request.promise.get.rethrow
 }
 
 object CommitQueue {
@@ -36,7 +47,12 @@ case class PartitionHandle[F[_]](data: async.mutable.Queue[F, ByteRecord], inter
     data.dequeue.interruptWhen(interruption.get)
 }
 object PartitionHandle {
-  def fromTopicPartition[F[_]](tp: TopicPartition): F[(TopicPartition, PartitionHandle[F])] = ???
+  def fromTopicPartition[F[_]: Effect](tp: TopicPartition)(
+    implicit ec: ExecutionContext): F[(TopicPartition, PartitionHandle[F])] =
+    for {
+      queue <- async.boundedQueue[F, ByteRecord](1000)
+      promise <- async.Promise.empty[F, Either[Throwable, Unit]]
+    } yield (tp, PartitionHandle(queue, promise))
 }
 
 case class ConsumerControl[F[_]](commitQueue: CommitQueue[F], records: Stream[F, ByteRecord])
@@ -44,25 +60,32 @@ case class PartitionedConsumerControl[F[_]](shutdown: F[Unit],
   records: Stream[F, (TopicPartition, Stream[F, ByteRecord])],
   commitQueue: CommitQueue[F])
 
-object Consuming {
+sealed trait Subscription
+object Subscription {
+  case class Topics(topics: List[String]) extends Subscription
+  case class Pattern(pattern: String) extends Subscription
+}
 
-  def createConsumer[F[_]: Sync](settings: Properties, subscription: String): F[ByteConsumer] =
+object Consuming {
+  def createConsumer[F[_]: Sync](settings: Properties): F[ByteConsumer] =
     Sync[F].delay(new ByteConsumer(settings))
 
-  def consumer[F[_]: Effect](settings: Properties, subscription: String)(
+  def consumer[F[_]: Effect](settings: Properties, subscription: Subscription,
+    maxPendingCommits: Int, pollInterval: FiniteDuration, pollTimeout: FiniteDuration)(
     implicit ec: ExecutionContext): F[ConsumerControl[F]] =
     for {
-      consumer <- createConsumer[F](settings, subscription)
-      _ <- Sync[F].delay(()) // TODO: Subscribe
-      commitQueue <- CommitQueue.create[F](???)
+      consumer <- createConsumer[F](settings)
+      _ <- subscribe(consumer, subscription, None)
+      commitQueue <- CommitQueue.create[F](maxPendingCommits)
       commits = commitQueue.queue.dequeue
-      polls = Stream.every(50.millis).as(ConsumerCommands.Poll)
+      polls = Stream.every(pollInterval).as(ConsumerCommands.Poll)
       res = commits.either(polls)
               .evalMap {
-                case Left(ConsumerCommands.CommitRequest(promise, _, _, _)) =>
-                  (commit(consumer, ???).void.attempt >>= promise.complete).as(List.empty[ByteRecord])
+                case Left(req) =>
+                  (commit(consumer, req.asOffsetMap).void.attempt >>= req.promise.complete)
+                    .as(List.empty[ByteRecord])
                 case Right(ConsumerCommands.Poll) =>
-                  poll(consumer, ???)
+                  poll(consumer, pollTimeout)
               }
               .flatMap(Stream.emits(_))
     } yield ConsumerControl(commitQueue, res)
@@ -79,14 +102,30 @@ object Consuming {
   def poll[F[_]: Sync](consumer: ByteConsumer, timeout: FiniteDuration): F[List[ByteRecord]] =
     Sync[F].delay(consumer.poll(timeout.toMillis).iterator().asScala.toList)
 
-  def partitionedConsumer[F[_]: ConcurrentEffect](settings: Properties, subscription: String)(implicit ec: ExecutionContext) =
+  def subscribe[F[_]: Sync](consumer: ByteConsumer, subscription: Subscription,
+    rebalanceListener: Option[ConsumerRebalanceListener]): F[Unit] = {
+      val listener = rebalanceListener.getOrElse(new NoOpConsumerRebalanceListener)
+
+      subscription match {
+        case Subscription.Topics(topics) =>
+          Sync[F].delay(consumer.subscribe(topics.asJava, listener))
+        case Subscription.Pattern(pattern) =>
+          for {
+            pattern <- Sync[F].delay(java.util.regex.Pattern.compile(pattern))
+            _ <- Sync[F].delay(consumer.subscribe(pattern, listener))
+          } yield ()
+      }
+    }
+
+  def partitionedConsumer[F[_]: ConcurrentEffect](settings: Properties,
+    subscription: Subscription, maxPendingCommits: Int, pollTimeout: FiniteDuration,
+    pollInterval: FiniteDuration)(implicit ec: ExecutionContext) =
     for {
       out <- async.unboundedQueue[F, (TopicPartition, Stream[F, ByteRecord])]
-      commitQueue <- CommitQueue.create[F](???)
+      commitQueue <- CommitQueue.create[F](maxPendingCommits)
       partitionTracker <- async.Ref[F, Map[TopicPartition, PartitionHandle[F]]](Map())
-      consumer <- createConsumer[F](settings, subscription)
-      _ <- Sync[F].delay {
-        val rebalancer = new ConsumerRebalanceListener {
+      consumer <- createConsumer[F](settings)
+      _ <- subscribe(consumer, subscription, Some(new ConsumerRebalanceListener {
           def onPartitionsAssigned(partitions: JCollection[TopicPartition]): Unit = {
             val handler = for {
               handles <- partitions.asScala.toList.traverse(PartitionHandle.fromTopicPartition[F])
@@ -94,7 +133,7 @@ object Consuming {
               _ <- handles.traverse { case (tp, h) => out.enqueue1((tp, h.records)) }
             } yield ()
 
-            Effect[F].runAsync(handler)(???).unsafeRunSync()
+            Effect[F].runAsync(handler)(_ => IO.unit).unsafeRunSync()
           }
 
           def onPartitionsRevoked(partitions: JCollection[TopicPartition]): Unit = {
@@ -104,23 +143,20 @@ object Consuming {
               _ <- handles.traverse_(_.interruption.complete(Right(())))
             } yield ()
 
-            Effect[F].runAsync(handler)(???).unsafeRunSync()
+            Effect[F].runAsync(handler)(_ => IO.unit).unsafeRunSync()
           }
-        }
-
-        consumer.subscribe(List(subscription).asJava, rebalancer)
-      }
+        }))
 
       commits = commitQueue.queue.dequeue
-      polls = Stream.every(50.millis).as(ConsumerCommands.Poll)
+      polls = Stream.every(pollInterval).as(ConsumerCommands.Poll)
       consumerDriver = commits.either(polls)
         .evalMap {
           case Left(req) =>
-            (commit(consumer, ???).void.attempt >>= req.promise.complete)
+            (commit(consumer, req.asOffsetMap).void.attempt >>= req.promise.complete)
               .void
           case Right(ConsumerCommands.Poll) =>
             for {
-              records <- poll(consumer, ???)
+              records <- poll(consumer, pollTimeout)
               tracker <- partitionTracker.get
               _ <- records.traverse_ { record =>
                      tracker
