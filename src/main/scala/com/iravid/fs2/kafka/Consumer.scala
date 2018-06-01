8da069e7b77b4cdfe67961e0063d0548ebc15594
Itamar Ravid <iravid@iravid.com>
2018-06-01 22:39:06 +0300

    Add shutdown to the plain consumer

--- src/main/scala/com/iravid/fs2/kafka/Consumer.scala
+++ src/main/scala/com/iravid/fs2/kafka/Consumer.scala
@@ -2,13 +2,15 @@ package com.iravid.fs2.kafka
 
 import cats.effect.Effect
 import cats.implicits._
-import fs2.Stream
+import fs2.{ async, Stream }
 import java.util.Properties
 import scala.concurrent.ExecutionContext
 
 import scala.concurrent.duration.FiniteDuration
 
-case class Consumer[F[_]](commitQueue: CommitQueue[F], records: Stream[F, ByteRecord])
+case class Consumer[F[_]](shutdown: F[Unit],
+                          commitQueue: CommitQueue[F],
+                          records: Stream[F, ByteRecord])
 
 object Consumer {
   import KafkaConsumerFunctions._
@@ -20,12 +22,14 @@ object Consumer {
     pollInterval: FiniteDuration,
     pollTimeout: FiniteDuration)(implicit ec: ExecutionContext): F[Consumer[F]] =
     for {
-      consumer    <- createConsumer[F](settings)
-      _           <- subscribe(consumer, subscription, None)
-      commitQueue <- CommitQueue.create[F](maxPendingCommits)
+      consumer       <- createConsumer[F](settings)
+      _              <- subscribe(consumer, subscription, None)
+      commitQueue    <- CommitQueue.create[F](maxPendingCommits)
+      shutdownSignal <- async.Promise.empty[F, Either[Throwable, Unit]]
+
       commits = commitQueue.queue.dequeue
       polls   = Stream.every(pollInterval).as(Poll)
-      res = commits
+      records = commits
         .either(polls)
         .evalMap {
           case Left(req) =>
@@ -35,5 +39,7 @@ object Consumer {
             poll(consumer, pollTimeout)
         }
         .flatMap(Stream.emits(_))
-    } yield Consumer(commitQueue, res)
+        .interruptWhen(shutdownSignal.get)
+
+    } yield Consumer(shutdownSignal.complete(Right(())), commitQueue, records)
 }
