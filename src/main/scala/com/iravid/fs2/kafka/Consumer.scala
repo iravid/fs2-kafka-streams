b8910fa420ee2b080d60bc7a9a00f929cb3391ed
Itamar Ravid <iravid@iravid.com>
2018-06-02 11:11:26 +0300

    Resource safety for the consumers

--- src/main/scala/com/iravid/fs2/kafka/Consumer.scala
+++ src/main/scala/com/iravid/fs2/kafka/Consumer.scala
@@ -1,6 +1,6 @@
 package com.iravid.fs2.kafka
 
-import cats.effect.{ Concurrent, ConcurrentEffect }
+import cats.effect.{ Concurrent, ConcurrentEffect, Sync }
 import cats.implicits._
 import fs2.{ async, Stream }
 import java.util.Properties
@@ -8,12 +8,39 @@ import scala.concurrent.ExecutionContext
 
 import scala.concurrent.duration.FiniteDuration
 
-case class Consumer[F[_]](shutdown: F[Unit],
-                          commitQueue: CommitQueue[F],
-                          records: Stream[F, ByteRecord])
+case class Consumer[F[_]](commitQueue: CommitQueue[F], records: Stream[F, ByteRecord])
 
 object Consumer {
   import KafkaConsumerFunctions._
+
+  def resources[F[_]: ConcurrentEffect](
+    settings: Properties,
+    maxPendingCommits: Int,
+    bufferSize: Int,
+    pollInterval: FiniteDuration,
+    pollTimeout: FiniteDuration)(implicit ec: ExecutionContext) =
+    for {
+      consumer    <- createConsumer[F](settings)
+      commitQueue <- CommitQueue.create[F](maxPendingCommits)
+      outQueue    <- async.boundedQueue[F, ByteRecord](bufferSize)
+      commits = commitQueue.queue.dequeue
+      polls   = Stream.every(pollInterval).as(Poll)
+      driver <- Concurrent[F].start {
+                 commits
+                   .either(polls)
+                   .evalMap {
+                     case Left(req) =>
+                       (commit(consumer, req.asOffsetMap).void.attempt >>= req.promise.complete).void
+                     case Right(Poll) =>
+                       for {
+                         records <- poll(consumer, pollTimeout)
+                         _       <- records.traverse_(outQueue.enqueue1)
+                       } yield ()
+                   }
+                   .compile
+                   .drain
+               }
+    } yield (commitQueue, outQueue, driver, consumer)
 
   def apply[F[_]: ConcurrentEffect](
     settings: Properties,
@@ -22,37 +49,25 @@ object Consumer {
     bufferSize: Int,
     pollInterval: FiniteDuration,
     pollTimeout: FiniteDuration)(implicit ec: ExecutionContext): F[Consumer[F]] =
-    for {
-      consumer    <- createConsumer[F](settings)
-      _           <- subscribe(consumer, subscription, None)
-      commitQueue <- CommitQueue.create[F](maxPendingCommits)
+    Stream
+      .bracket(resources[F](settings, maxPendingCommits, bufferSize, pollInterval, pollTimeout))(
+        {
+          case (commitQueue, outQueue, _, consumer) =>
+            Stream.eval {
+              subscribe(consumer, subscription, None)
+                .as(Consumer(commitQueue, outQueue.dequeue))
+            }
+        }, {
+          case (_, _, driver, consumer) =>
+            for {
+              _ <- Sync[F].delay(consumer.unsubscribe())
+              _ <- driver.cancel
+              _ <- Sync[F].delay(consumer.close())
+            } yield ()
+        }
+      )
+      .compile
+      .toList
+      .map(_.last)
 
-      outQueue    <- async.boundedQueue[F, ByteRecord](bufferSize)
-      shutdownSig <- async.Promise.empty[F, Either[Throwable, Unit]]
-      records = outQueue.dequeue.interruptWhen(shutdownSig.get)
-
-      commits = commitQueue.queue.dequeue
-      polls   = Stream.every(pollInterval).as(Poll)
-      consumerFiber <- Concurrent[F].start {
-                        commits
-                          .either(polls)
-                          .evalMap {
-                            case Left(req) =>
-                              (commit(consumer, req.asOffsetMap).void.attempt >>= req.promise.complete).void
-                            case Right(Poll) =>
-                              for {
-                                records <- poll(consumer, pollTimeout)
-                                _       <- records.traverse_(outQueue.enqueue1)
-                              } yield ()
-                          }
-                          .compile
-                          .drain
-                      }
-
-      interruptEverything = for {
-        _ <- consumerFiber.cancel
-        _ <- shutdownSig.complete(Right(()))
-      } yield ()
-
-    } yield Consumer(interruptEverything, commitQueue, records)
 }
--- src/main/scala/com/iravid/fs2/kafka/PartitionedConsumer.scala
+++ src/main/scala/com/iravid/fs2/kafka/PartitionedConsumer.scala
@@ -10,9 +10,8 @@ import scala.collection.JavaConverters._
 import scala.concurrent.duration._
 import scala.concurrent.ExecutionContext
 
-case class PartitionedConsumer[F[_]](shutdown: F[Unit],
-                                     records: Stream[F, (TopicPartition, Stream[F, ByteRecord])],
-                                     commitQueue: CommitQueue[F])
+case class PartitionedConsumer[F[_]](commitQueue: CommitQueue[F],
+                                     records: Stream[F, (TopicPartition, Stream[F, ByteRecord])])
 
 object PartitionedConsumer {
   import KafkaConsumerFunctions._
@@ -32,6 +31,74 @@ object PartitionedConsumer {
       } yield (tp, PartitionHandle(queue, promise))
   }
 
+  def resources[F[_]: ConcurrentEffect](
+    settings: Properties,
+    maxPendingCommits: Int,
+    partitionBufferSize: Int,
+    pollTimeout: FiniteDuration,
+    pollInterval: FiniteDuration
+  )(implicit ec: ExecutionContext) =
+    for {
+      consumer         <- createConsumer[F](settings)
+      commitQueue      <- CommitQueue.create[F](maxPendingCommits)
+      partitionTracker <- async.boundedQueue[F, Map[TopicPartition, PartitionHandle[F]]](1)
+      _                <- partitionTracker.enqueue1(Map())
+      partitionsOut    <- async.unboundedQueue[F, (TopicPartition, Stream[F, ByteRecord])]
+
+      rebalanceListener = new ConsumerRebalanceListener {
+        def onPartitionsAssigned(partitions: JCollection[TopicPartition]): Unit = {
+          val handler = for {
+            handles <- partitions.asScala.toList
+                        .traverse(PartitionHandle.fromTopicPartition[F](_, partitionBufferSize))
+            _ <- partitionTracker.dequeue1
+                  .map(_ ++ handles)
+                  .flatMap(partitionTracker.enqueue1)
+            _ <- handles.traverse {
+                  case (tp, h) => partitionsOut.enqueue1((tp, h.records))
+                }
+          } yield ()
+
+          Effect[F].runAsync(handler)(_ => IO.unit).unsafeRunSync()
+        }
+
+        def onPartitionsRevoked(jpartitions: JCollection[TopicPartition]): Unit = {
+          val handler = for {
+            tracker <- partitionTracker.dequeue1
+            partitions = jpartitions.asScala.toList
+            handles    = partitions.flatMap(tracker.get)
+            _ <- handles.traverse_(_.interruption.complete(Right(())))
+            _ <- partitionTracker.enqueue1(tracker -- partitions)
+          } yield ()
+
+          Effect[F].runAsync(handler)(_ => IO.unit).unsafeRunSync()
+        }
+      }
+
+      commits = commitQueue.queue.dequeue
+      polls   = Stream.every(pollInterval).as(Poll)
+      driver <- Concurrent[F].start {
+                 commits
+                   .either(polls)
+                   .evalMap {
+                     case Left(req) =>
+                       (commit(consumer, req.asOffsetMap).void.attempt >>= req.promise.complete).void
+                     case Right(Poll) =>
+                       for {
+                         records <- poll(consumer, pollTimeout)
+                         tracker <- partitionTracker.dequeue1
+                         _ <- records.traverse_ { record =>
+                               tracker
+                                 .get(new TopicPartition(record.topic, record.partition))
+                                 .traverse_(_.data.enqueue1(record))
+                             }
+                         _ <- partitionTracker.enqueue1(tracker)
+                       } yield ()
+                   }
+                   .compile
+                   .drain
+               }
+    } yield (commitQueue, partitionsOut, driver, consumer, rebalanceListener)
+
   def apply[F[_]: ConcurrentEffect](
     settings: Properties,
     subscription: Subscription,
@@ -39,64 +106,25 @@ object PartitionedConsumer {
     partitionBufferSize: Int,
     pollTimeout: FiniteDuration,
     pollInterval: FiniteDuration)(implicit ec: ExecutionContext): F[PartitionedConsumer[F]] =
-    for {
-      out              <- async.unboundedQueue[F, (TopicPartition, Stream[F, ByteRecord])]
-      commitQueue      <- CommitQueue.create[F](maxPendingCommits)
-      partitionTracker <- async.Ref[F, Map[TopicPartition, PartitionHandle[F]]](Map())
-      consumer         <- createConsumer[F](settings)
-      _ <- subscribe(
-            consumer,
-            subscription,
-            Some(new ConsumerRebalanceListener {
-              def onPartitionsAssigned(partitions: JCollection[TopicPartition]): Unit = {
-                val handler = for {
-                  handles <- partitions.asScala.toList
-                              .traverse(
-                                PartitionHandle.fromTopicPartition[F](_, partitionBufferSize))
-                  _ <- partitionTracker.modify(_ ++ handles)
-                  _ <- handles.traverse { case (tp, h) => out.enqueue1((tp, h.records)) }
-                } yield ()
-
-                Effect[F].runAsync(handler)(_ => IO.unit).unsafeRunSync()
-              }
-
-              def onPartitionsRevoked(partitions: JCollection[TopicPartition]): Unit = {
-                val handler = for {
-                  tracker <- partitionTracker.get
-                  handles = partitions.asScala.toList.flatMap(tracker.get)
-                  _ <- handles.traverse_(_.interruption.complete(Right(())))
-                } yield ()
-
-                Effect[F].runAsync(handler)(_ => IO.unit).unsafeRunSync()
-              }
-            })
-          )
-
-      commits = commitQueue.queue.dequeue
-      polls   = Stream.every(pollInterval).as(Poll)
-      consumerDriver = commits
-        .either(polls)
-        .evalMap {
-          case Left(req) =>
-            (commit(consumer, req.asOffsetMap).void.attempt >>= req.promise.complete).void
-          case Right(Poll) =>
+    Stream
+      .bracket(
+        resources(settings, maxPendingCommits, partitionBufferSize, pollTimeout, pollInterval))(
+        {
+          case (commitQueue, partitionsOut, _, consumer, rebalanceListener) =>
+            Stream.eval {
+              subscribe(consumer, subscription, Some(rebalanceListener))
+                .as(PartitionedConsumer(commitQueue, partitionsOut.dequeue))
+            }
+        }, {
+          case (_, _, driver, consumer, _) =>
             for {
-              records <- poll(consumer, pollTimeout)
-              tracker <- partitionTracker.get
-              _ <- records.traverse_ { record =>
-                    tracker
-                      .get(new TopicPartition(record.topic, record.partition))
-                      .traverse_(_.data.enqueue1(record))
-                  }
+              _ <- Sync[F].delay(consumer.unsubscribe())
+              _ <- driver.cancel
+              _ <- Sync[F].delay(consumer.close())
             } yield ()
         }
-        .compile
-        .drain
-      fiber <- Concurrent[F].start(consumerDriver)
-      interruptEverything = for {
-        _       <- fiber.cancel
-        tracker <- partitionTracker.get
-        _       <- tracker.values.toList.traverse(_.interruption.complete(Right(())))
-      } yield ()
-    } yield PartitionedConsumer(interruptEverything, out.dequeue, commitQueue)
+      )
+      .compile
+      .toList
+      .map(_.last)
 }
