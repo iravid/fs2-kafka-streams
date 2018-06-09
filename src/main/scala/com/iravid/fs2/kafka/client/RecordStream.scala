4cef7b20c16b457a13b036562533c0f2f5dd6cdd
Itamar Ravid <iravid@iravid.com>
2018-06-09 09:13:59 +0300

    Use unNoneTerminate for the plain RecordStream too

--- src/main/scala/com/iravid/fs2/kafka/client/RecordStream.scala
+++ src/main/scala/com/iravid/fs2/kafka/client/RecordStream.scala
@@ -13,48 +13,57 @@ case class RecordStream[F[_], T](commitQueue: CommitQueue[F],
                                  records: Stream[F, ConsumerMessage[Result, T]])
 
 object RecordStream {
-  def resources[F[_]: ConcurrentEffect](settings: ConsumerSettings, consumer: Consumer[F])(
-    implicit timer: Timer[F]) =
+  def resources[F[_], T: KafkaDecoder](settings: ConsumerSettings, consumer: Consumer[F])(
+    implicit F: ConcurrentEffect[F],
+    timer: Timer[F]) =
     for {
       commitQueue         <- CommitQueue.create[F](settings.maxPendingCommits)
-      outQueue            <- async.boundedQueue[F, ByteRecord](settings.outputBufferSize)
+      outQueue            <- async.boundedQueue[F, Option[ByteRecord]](settings.outputBufferSize)
       pollingLoopShutdown <- Deferred[F, Either[Throwable, Unit]]
-      commits = commitQueue.queue.dequeue
-      polls   = Stream(Poll) ++ Stream.repeatEval(timer.sleep(settings.pollInterval).as(Poll))
-      _ <- Concurrent[F].start {
-            commits
-              .either(polls)
-              .interruptWhen(pollingLoopShutdown.get)
-              .evalMap {
-                case Left((deferred, req)) =>
-                  (consumer.commit(req.asOffsetMap).void.attempt >>= deferred.complete).void
-                case Right(Poll) =>
-                  for {
-                    records <- consumer.poll(settings.pollTimeout, settings.wakeupTimeout)
-                    _       <- records.traverse_(outQueue.enqueue1)
-                  } yield ()
-              }
-              .compile
-              .drain
-          }
-    } yield (commitQueue, outQueue, pollingLoopShutdown.complete(Right(())))
+      shutdownQueue       <- async.boundedQueue[F, None.type](1)
+      commits       = commitQueue.queue.dequeue
+      polls         = Stream(Poll) ++ Stream.repeatEval(timer.sleep(settings.pollInterval).as(Poll))
+      commandStream = shutdownQueue.dequeue.merge(commits.either(polls).map(_.some))
+      pollingLoop <- Concurrent[F].start {
+                      commandStream.unNoneTerminate
+                        .evalMap {
+                          case Left((deferred, req)) =>
+                            (consumer
+                              .commit(req.asOffsetMap)
+                              .void
+                              .attempt >>= deferred.complete).void
+                          case Right(Poll) =>
+                            for {
+                              records <- consumer.poll(settings.pollTimeout, settings.wakeupTimeout)
+                              _       <- records.traverse_(rec => outQueue.enqueue1(rec.some))
+                            } yield ()
+                        }
+                        .compile
+                        .drain
+                    }
+      outputStream = outQueue.dequeue.unNoneTerminate
+        .through(deserialize[F, T])
+      performShutdown = shutdownQueue.enqueue1(None) *>
+        outQueue.enqueue1(None) *>
+        pollingLoop.join
+    } yield (commitQueue, outputStream, performShutdown)
 
   def apply[F[_]: ConcurrentEffect, T: KafkaDecoder](
     settings: ConsumerSettings,
     consumer: Consumer[F],
     subscription: Subscription)(implicit timer: Timer[F]): Resource[F, RecordStream[F, T]] =
-    Resource.make(resources[F](settings, consumer)) {
+    Resource.make(resources[F, T](settings, consumer)) {
       case (_, _, shutdown) =>
         for {
-          _ <- consumer.unsubscribe
           _ <- shutdown
+          _ <- consumer.unsubscribe
         } yield ()
     } flatMap {
-      case (commitQueue, outQueue, _) =>
+      case (commitQueue, outputStream, _) =>
         Resource.liftF {
           consumer
             .subscribe(subscription, _ => Sync[F].unit)
-            .as(RecordStream(commitQueue, outQueue.dequeue through deserialize[F, T]))
+            .as(RecordStream(commitQueue, outputStream))
         }
     }
 
